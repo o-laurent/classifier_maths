5e91c50302e30fc8b94e5a832d6979d6ac978900
Olivier Laurent <olivier.ar.laurent@gmail.com>
2022-02-16 17:06:12 +0100

    improve accuracy computation

--- src/evaluate_accuracy.cu
+++ src/evaluate_accuracy.cu
@@ -34,36 +34,7 @@ const char *_cudaGetErrorEnum(cublasStatus_t error)
     return "<unknown>";
 }
 
-/* Start computing the softmax on the device */
-__global__ void log_kernel(float *Z_d, int nb_LigneZ, int nb_ColZ, float *d_logP)
-{
-    int case_id = blockIdx.x * blockDim.x + threadIdx.x;
-    int i = case_id % nb_LigneZ; // line id
-    int j = case_id / nb_LigneZ; // col id
-
-    /* If j is coherent */
-    if (j < nb_ColZ)
-    {
-        /* Replace by the log */
-        d_logP[IDX2C(i, j, nb_LigneZ)] = logf(Z_d[IDX2C(i, j, nb_LigneZ)]);
-    }
-}
-
-/* Compute the sum of the diagonal of the product of A and B sum(diag(Yt*P)) */
-__global__ void sum_diag_kernel(fmatrix d_A, float *J)
-{
-    int case_id = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = case_id;
-    int i = case_id;
-
-    /* If j is coherent */
-    if (j < d_A.cols && i < d_A.rows)
-    {
-        atomicAdd(J, getfm(d_A, i, j));
-    }
-}
-
-__global__ void evaluate_accuracy_kernel(fmatrix d_Y, fmatrix d_Z, int *count)
+static __global__ void evaluate_accuracy_kernel(fmatrix d_Y, fmatrix d_Z, int *count)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < d_Z.cols)
@@ -92,19 +63,20 @@ float evaluate_accuracy(cublasHandle_t handle, fmatrix d_W, fmatrix d_X, fmatrix
     fmatrix_assert(d_Z);
 
     float alpha = 1.0f;
-    float beta = 1.0f;
+    float beta = 0.0f;
     if (verbose)
     {
         printf("dw rows %d, dw cols %d, dX rows %d, dX cols %d, dZ rows %d, dZ cols %d\n", d_W.rows, d_W.cols, d_X.rows, d_X.cols, d_Z.rows, d_Z.cols);
         printf("m %d, n %d, k %d\n", d_W.cols, d_X.cols, d_W.rows);
     }
+    /* Z = W^T x X */
     cublasStatus_t multstat = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, d_W.cols, d_X.cols, d_W.rows, &alpha, d_W.data, d_W.rows, d_X.data, d_X.rows, &beta, d_Z.data, d_Z.rows);
+    gpuErrchk(cudaPeekAtLastError());
 
     if (multstat != CUBLAS_STATUS_SUCCESS)
     {
         printf("CUBLAS matrix multiplication failed 1\n");
         printf("%s\n", _cudaGetErrorEnum(multstat));
-        gpuErrchk(cudaPeekAtLastError());
     }
 
     int true_class = 0;
@@ -114,6 +86,7 @@ float evaluate_accuracy(cublasHandle_t handle, fmatrix d_W, fmatrix d_X, fmatrix
     gpuErrchk(
         cudaMemcpy(d_count, &true_class, sizeof(int), cudaMemcpyHostToDevice));
 
+    /* One thread per column */
     int threadsPerBlock = d_Z.cols;
     int blocksPerGrid = 1;
     if (threadsPerBlock > THREADS_PER_BLOCK)
@@ -121,6 +94,7 @@ float evaluate_accuracy(cublasHandle_t handle, fmatrix d_W, fmatrix d_X, fmatrix
         blocksPerGrid = (threadsPerBlock - 1) / THREADS_PER_BLOCK + 1;
         threadsPerBlock = THREADS_PER_BLOCK;
     }
+
     evaluate_accuracy_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_Y, d_Z, d_count);
     device_synchronize();
     gpuErrchk(cudaPeekAtLastError());
@@ -145,14 +119,14 @@ float evaluate_logloss(cublasHandle_t handle, fmatrix d_P, fmatrix d_Y, bool ver
 
     /* One thread per element */
     int thread_nb = d_P.rows * d_P.cols;
-    dim3 dimGrid(1 + (thread_nb / THREADS_PER_BLOCK));
-    dim3 dimBlock(THREADS_PER_BLOCK);
+    int dimGrid(1 + (thread_nb / THREADS_PER_BLOCK));
+    int dimBlock(THREADS_PER_BLOCK);
 
     /* Create the matrix which will contain the log of P */
     fmatrix d_logP = fmatrix_create_on_device(d_P.rows, d_P.cols);
 
     /* Compute the log */
-    log_kernel<<<dimGrid, dimBlock>>>(d_P.data, d_P.rows, d_P.cols, d_logP.data);
+    log_kernel<<<dimGrid, dimBlock>>>(d_P, d_logP.data);
     gpuErrchk(cudaPeekAtLastError());
 
     fmatrix d_Z = fmatrix_create_on_device(d_Y.cols, d_P.cols);
@@ -163,13 +137,14 @@ float evaluate_logloss(cublasHandle_t handle, fmatrix d_P, fmatrix d_Y, bool ver
 
     float alpha = -1.0f;
     float beta = 0.0f;
-    // dZ =dY^T*dP
 
     if (verbose)
     {
         printf("dY rows %d, dY cols %d, dP rows %d, dP cols %d, dZ rows %d, dZ cols %d\n", d_Y.rows, d_Y.cols, d_P.rows, d_P.cols, d_Z.rows, d_Z.cols);
         printf("m %d, n %d, k %d\n", d_Y.cols, d_P.cols, d_Y.rows);
     }
+
+    // dZ =dY^T*dP
     cublasStatus_t multstat = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, d_Y.cols, d_logP.cols, d_Y.rows, &alpha, d_Y.data, d_Y.rows, d_logP.data, d_logP.rows, &beta, d_Z.data, d_Z.rows);
     if (multstat != CUBLAS_STATUS_SUCCESS)
     {
@@ -179,13 +154,15 @@ float evaluate_logloss(cublasHandle_t handle, fmatrix d_P, fmatrix d_Y, bool ver
     }
 
     /* One thread per column */
-    thread_nb = d_Z.cols;
-    dimGrid = dim3(1 + (thread_nb / THREADS_PER_BLOCK));
-    dimBlock = dim3(THREADS_PER_BLOCK);
+    dimBlock = d_Z.cols;
+    dimGrid = 1;
+    if (dimBlock > THREADS_PER_BLOCK)
+    {
+        dimGrid = (dimBlock - 1) / THREADS_PER_BLOCK + 1;
+        dimBlock = THREADS_PER_BLOCK;
+    }
 
-    fmatrix_assert(d_Z);
-
-    sum_diag_kernel<<<dimGrid, dimBlock>>>(d_Z, d_J); //d_Z.rows, d_Z.cols, &J);
+    sum_diag_kernel<<<dimGrid, dimBlock>>>(d_Z, d_J);
     gpuErrchk(cudaPeekAtLastError());
 
     if (verbose)
