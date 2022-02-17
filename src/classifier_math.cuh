50a4aab975c40601d5d55b459219958a39660b6d
Olivier Laurent <olivier.ar.laurent@gmail.com>
2022-02-17 16:27:30 +0100

    following + seed

--- src/classifier_math.cuh
+++ src/classifier_math.cuh
@@ -4,7 +4,7 @@
 #include "fmatrix.cuh"
 
 /* Compute the elementwise logarithm on the device */
-__global__ void log_kernel(fmatrix Z_d, float *d_logP);
+__global__ void log_kernel(fmatrix Z_d, fmatrix d_logP);
 
 /* Compute the sum of the diagonal of the product of A and B sum(diag(Yt*P)) */
 __global__ void sum_diag_kernel(fmatrix d_A, float *J);
@@ -13,7 +13,7 @@ __global__ void sum_diag_kernel(fmatrix d_A, float *J);
 float float_rand(float min, float max);
 
 /* Initialize W with Xavier's method, scaled by a. */
-void xavier_weight_init(float a, fmatrix W);
+void xavier_weight_init(float a, fmatrix W, unsigned seed = 42);
 
 /* Compute the softmax for each column of Z and store in P */
 fmatrix softmax_col(fmatrix Z);
